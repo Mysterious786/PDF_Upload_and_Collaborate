54179a4c57a0f32d913e349c796504da3af887cf
DESKTOP-GUGSNQ4 <saqlainzarjisansari@gmail.com>
2025-04-18 22:45:59 +0530

    fix: fixed max pdf upload size

--- app/dashboard/upload/page.tsx
+++ app/dashboard/upload/page.tsx
@@ -50,7 +50,7 @@ export default function UploadPdf() {
         setFile(droppedFile)
       } else {
         toast({
-          title: "Invalid file type",
+          title: "Invalid file type/Check file size limit is 9MB max",
           description: "Please upload a PDF file",
           variant: "destructive",
         })
